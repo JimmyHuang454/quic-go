2778fa3515941760e825f20e32b1d6d72cba7f74
世界 <i@sekai.icu>
2023-09-11 16:23:07 +0800

    Don't save conn to global var

--- transport.go
+++ transport.go
@@ -228,7 +228,7 @@ func (t *Transport) init(allowZeroLengthConnIDs bool) error {
 			t.connIDGenerator = &protocol.DefaultConnectionIDGenerator{ConnLen: t.connIDLen}
 		}
 
-		getMultiplexer().AddConn(t.Conn)
+		//getMultiplexer().AddConn(t.Conn)
 		go t.listen(conn)
 		go t.runSendQueue()
 	})
@@ -319,7 +319,7 @@ func (t *Transport) close(e error) {
 
 func (t *Transport) listen(conn rawConn) {
 	defer close(t.listening)
-	defer getMultiplexer().RemoveConn(t.Conn)
+	//defer getMultiplexer().RemoveConn(t.Conn)
 
 	for {
 		p, err := conn.ReadPacket()
