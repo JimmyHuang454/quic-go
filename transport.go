8222d7fce0e355ada9bba157c3d9d8d200b8a9f4
世界 <i@sekai.icu>
2023-09-19 18:10:36 +0800

    Reformat code

--- transport.go
+++ transport.go
@@ -182,7 +182,7 @@ func (t *Transport) dial(ctx context.Context, addr net.Addr, host string, tlsCon
 	}
 	tlsConf = tlsConf.Clone()
 	tlsConf.MinVersion = tls.VersionTLS13
-	//setTLSConfigServerName(tlsConf, addr, host)
+	// setTLSConfigServerName(tlsConf, addr, host)
 	return dial(ctx, newSendConn(t.conn, addr, packetInfo{}, utils.DefaultLogger), t.connIDGenerator, t.handlerMap, tlsConf, conf, onClose, use0RTT)
 }
 
@@ -228,7 +228,7 @@ func (t *Transport) init(allowZeroLengthConnIDs bool) error {
 			t.connIDGenerator = &protocol.DefaultConnectionIDGenerator{ConnLen: t.connIDLen}
 		}
 
-		//getMultiplexer().AddConn(t.Conn)
+		// getMultiplexer().AddConn(t.Conn)
 		go t.listen(conn)
 		go t.runSendQueue()
 	})
@@ -319,7 +319,7 @@ func (t *Transport) close(e error) {
 
 func (t *Transport) listen(conn rawConn) {
 	defer close(t.listening)
-	//defer getMultiplexer().RemoveConn(t.Conn)
+	// defer getMultiplexer().RemoveConn(t.Conn)
 
 	for {
 		p, err := conn.ReadPacket()
